88ffe7ad2218a549d890c594e2552470049fccba
katejsenger <katejsenger@yahoo.com>
2018-11-15 17:01:39 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,17 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-
+  monopoloy = {
+    :railroads=>
+      {
+        :pieces=>4,
+        names: {},
+        rent_in_dollars:
+        {
+          one_piece_owned: 25
+        }
+      }
+}
 
 end
 
